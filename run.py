e19493171421bb286c13ba60776fdf10d7b5a003
Maurizio <maurizio.loffredo@outlook.com>
2022-11-24 14:48:17 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,57 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Simplified version of 'Battleship' (game),
+running in the terminal and featuring 1x1-sized ships.
+"""
+
+# Importing of common tools needed in the following:
+from string import ascii_uppercase
+from random import randint
+
+# Global variables:
+OUTCOMES = ('Hit!', 'Miss!')
+SCORES = {'computer': 0, 'player': 0}
+
+
+# Board class, representing the parent class for boards:
+class Board:
+    """
+    It sets the board sizes, the number of ships (fleet),
+    the player's name and the board type (player's, computer's).
+    It has methods for printing boards (player's, computer's),
+    adding its own ships and opponent's guesses.
+    """
+
+    def __init__(self, size, fleet, name, type):
+        self.size = size
+        self.fleet = fleet
+        self.name = name
+        self.type = type
+        self.board = [['□' for x in range(size)] for y in range(size)]
+        self.columns_dictionary = dict(zip(ascii_uppercase, range(size)))
+        self.columns_string = '  ' + ' '.join(list(self.columns_dictionary))
+        self.ships = []
+        self.guesses = []
+
+    def print_board(self):
+        print(self.columns_string)
+        row_number = 0
+
+        for row in self.board:
+            print('%d %s' % (row_number, ' '.join(row)))
+            row_number += 1
+
+    def add_ship(self, row, column):
+        self.ships.append((row, column))
+
+        if self.type == 'player':
+            self.board[row][column] = '■'
+
+    def add_guess(self, row, column):
+        self.guesses.append((row, column))
+        self.board[row][column] = 'x'
+
+        if (row, column) in self.ships:
+            self.board[row][column] = '#'
+            return OUTCOMES[0]
+        else:
+            return OUTCOMES[1]
